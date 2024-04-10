6de7fe6bc55244554947851a5acabed906fa3b9f
avanthik33 <avanthik33@gmail.com>
2024-04-10 20:10:49 +0530

    .\.git\

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors()); // Enable Cross-Origin Resource Sharing
 app.use(express.json()); // Parse JSON request bodies
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://archana2002:archana07@cluster0.zzcjkzd.mongodb.net/visitorDb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://archana2002:archana07@cluster0.zzcjkzd.mongodb.net/ChittiDb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Error connecting to MongoDB:', err));
 
